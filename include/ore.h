60d6de9cc75534bf5d571bd57736f805f9b6c54b
adam <nassimdjahnit@yahoo.fr>
2025-03-24 10:53:16 +0100

    premiere modification

--- include/ore.h
+++ include/ore.h
@@ -1,7 +1,7 @@
 #ifndef ORE_H
 #define ORE_H
 
-typedef enum { FUEL, ORE1, ORE2, ORE3, EMPTY } Ore; //Types de minerais
+typedef enum { FUEL, ORE1, ORE2, ORE3, EMPTY } Ore; // Types de minerais
 
 typedef struct {
     Ore ore;
