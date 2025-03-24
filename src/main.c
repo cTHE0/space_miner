a091c3cc8a0bf2f5d5fad87aaab56c602b0780da
cTHE0 <theo.couerbe@gmail.com>
2025-03-24 16:40:22 +0100

    testo

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 #include "event.h"
 #include "renderer.h"
 #include "ship.h"
-//c'est Adam
+
 #define INIT_PLANET_COUNT 50
 #define INIT_SHIP_COUNT 10
-//c'est pas ADAN
+
 int main() {
     int planet_count = INIT_PLANET_COUNT;
     int ship_count = INIT_SHIP_COUNT;
