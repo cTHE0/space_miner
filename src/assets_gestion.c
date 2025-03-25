1f5c0e851974a8b2412f524bfeade2b118f739a4
adam-nahal <adamnahal@laposte.net>
2025-03-26 00:40:33 +0100

    organisation assets

--- src/assets_gestion.c
+++ src/assets_gestion.c
@@ -5,10 +5,9 @@
 
 SDL_Texture **loadTextures(SDL_Renderer *renderer) {
     SDL_Texture **textures = malloc(PNG_IMAGE_NUMBER * sizeof(SDL_Texture*));
-    char pngNames[PNG_IMAGE_NUMBER][128] = {"assets/img/fuseegif.png",  // 128 : limite de caractères par chaine de caractère
-                                            "assets/img/fusee.png",
-                                            "assets/img/bouton_ajouter.png",
-                                            "assets/img/attaquant.png",
+    char pngNames[PNG_IMAGE_NUMBER][128] = {"assets/img/ships/s1.png",  // 128 : limite de caractères par chaine de caractère
+                                            "assets/img/ships/s2.png",
+                                            "assets/img/ships/s3.png",
                                             "assets/img/planetes/p1.png",
                                             "assets/img/planetes/p2.png",
                                             "assets/img/planetes/p3.png",
@@ -18,7 +17,9 @@ SDL_Texture **loadTextures(SDL_Renderer *renderer) {
                                             "assets/img/planetes/p7.png",
                                             "assets/img/planetes/p8.png",
                                             "assets/img/planetes/p9.png",
-                                            "assets/img/planetes/p10.png"};
+                                            "assets/img/planetes/p10.png",
+                                            "assets/img/maps/m1.png",
+                                            "assets/img/others/add_button.png"};
 
     for (int i = 0; i < PNG_IMAGE_NUMBER; i++) {
         textures[i] = IMG_LoadTexture(renderer, pngNames[i]);
