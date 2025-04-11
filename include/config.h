e0f87011d79b3ab648e39a8c6ec32c3e1bfb1ee4
cTHE0 <theo.couerbe@gmail.com>
2025-04-11 10:08:12 +0200

    Agrandissement map

--- include/config.h
+++ include/config.h
@@ -3,11 +3,11 @@
 
 #define SCREEN_WIDTH 1920 // (autre 16/9 courant plus petit : 1280 par 720)
 #define SCREEN_HEIGHT 1080
-#define MAP_SIZE 3500
+#define MAP_SIZE 10500
 
 
-#define INIT_PLANET_COUNT 5
-#define INIT_SHIP_COUNT 5
+#define INIT_PLANET_COUNT 50
+#define INIT_SHIP_COUNT 10000
 #define CST_TEXT_NUMBER 4
 
 
--- src/map.c
+++ src/map.c
@@ -6,11 +6,17 @@
 void renderMap(SDL_Renderer *renderer, SDL_Texture *textureMap) {
     SDL_Rect destRect;
     
-    // Appliquer le décalage et le zoom de la caméra
-    destRect.x = ((0 - camera.rect.x - SCREEN_WIDTH / 2.f) * camera.scale + SCREEN_WIDTH / 2.f);
-    destRect.y = ((0 - camera.rect.y - SCREEN_HEIGHT / 2.f) * camera.scale + SCREEN_HEIGHT / 2.f);
-    destRect.w = MAP_SIZE * camera.scale;  // Largeur adaptée au zoom
-    destRect.h = MAP_SIZE * camera.scale;  // Hauteur adaptée au zoom
+    
+    destRect.w = MAP_SIZE * camera.scale / 3 ;  // Largeur adaptée au zoom
+    destRect.h = MAP_SIZE * camera.scale / 3;  // Hauteur adaptée au zoom
 
-    SDL_RenderCopy(renderer, textureMap, NULL, &destRect);
+    for (int i = 0; i<3; i++){
+        for (int j = 0; j<3; j++){
+            // Appliquer le décalage et le zoom de la caméra
+            destRect.x = ((i*3500 - camera.rect.x - SCREEN_WIDTH / 2.f) * camera.scale + SCREEN_WIDTH / 2.f);
+            destRect.y = ((j*3500 - camera.rect.y - SCREEN_HEIGHT / 2.f) * camera.scale + SCREEN_HEIGHT / 2.f);
+
+            SDL_RenderCopy(renderer, textureMap, NULL, &destRect);
+        }
+    }
 }
