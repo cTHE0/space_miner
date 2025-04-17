dc1d18ae57452a1866fa7f3a3658306fba7fc1c0
adam-nahal <adamnahal@laposte.net>
2025-04-17 15:44:40 +0200

    Theo qui abuse sur la dimension de la map : 700,000 px de large ???

--- src/map.c
+++ src/map.c
@@ -8,14 +8,14 @@ void renderMap(SDL_Renderer *renderer, SDL_Texture *textureMap) {
     SDL_Rect destRect;
 
     if (camera.scale > 0.35 ) {
-        destRect.w = MAP_SIZE * camera.scale / 192 ;  // Largeur adaptée au zoom
-        destRect.h = MAP_SIZE * camera.scale / 192;  // Hauteur adaptée au zoom
+        destRect.w = MAP_SIZE * camera.scale / 100 ;  // Largeur adaptée au zoom
+        destRect.h = MAP_SIZE * camera.scale / 100;  // Hauteur adaptée au zoom
 
-        for (int i = 0; i < 192; i++){
-            for (int j = 0; j < 192; j++){
+        for (int i = 0; i < 100; i++){
+            for (int j = 0; j < 100; j++){
                 // Appliquer le décalage et le zoom de la caméra
-                destRect.x = ((i*3500 - camera.rect.x - SCREEN_WIDTH / 2.f) * camera.scale + SCREEN_WIDTH / 2.f);
-                destRect.y = ((j*3500 - camera.rect.y - SCREEN_HEIGHT / 2.f) * camera.scale + SCREEN_HEIGHT / 2.f);
+                destRect.x = ((i * 3500 - camera.rect.x - SCREEN_WIDTH / 2.f) * camera.scale + SCREEN_WIDTH / 2.f);
+                destRect.y = ((j * 3500 - camera.rect.y - SCREEN_HEIGHT / 2.f) * camera.scale + SCREEN_HEIGHT / 2.f);
 
                 SDL_RenderCopy(renderer, textureMap, NULL, &destRect);
             }
