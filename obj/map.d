bd55735699dca9c303501487ec7c4a7ac37619b1
cTHE0 <theo.couerbe@gmail.com>
2025-04-26 13:52:16 +0200

    Sauvegarde temporaire avant retour à un ancien commit

--- obj/map.d
+++
@@ -1,9 +0,0 @@
-obj/map.o: src/map.c inc/map.h inc/camera.h inc/planet.h inc/config.h \
- inc/renderer.h inc/ship.h inc/ore.h
-inc/map.h:
-inc/camera.h:
-inc/planet.h:
-inc/config.h:
-inc/renderer.h:
-inc/ship.h:
-inc/ore.h:
