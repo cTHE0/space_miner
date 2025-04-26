6c515e7b948de2c67b5568efa03b26b8ba666582
cTHE0 <theo.couerbe@gmail.com>
2025-04-26 14:07:54 +0200

    Début ajout basicShipWindow

--- src/basic_ship_window.c
+++ src/basic_ship_window.c
@@ -17,10 +17,9 @@ void displayBasicShipWindow(SDL_Texture ***imageTextures, Ship *ships){
     if (getWindowType() != BASIC_SHIP_WINDOW) {  // La fenetre d'informations [basiques] d'une fusee est-elle ouverte ?
         return;
     }
-    printf("%d", idShip);
     SDL_Rect shipDestRect = getShipOnScreen(ships[idShip]);
 
     rangeCircle = (SDL_Rect){shipDestRect.x - ships[idShip].range, shipDestRect.y - ships[idShip].range, 2*ships[idShip].range, 2*ships[idShip].range};
-    SDL_RenderCopy(renderer, imageTextures[4][4], NULL, &rangeCircle);
+    SDL_RenderCopy(renderer, imageTextures[5][4], NULL, &rangeCircle);
     // + 1% des parts à la personne qui lira ce message
 }
